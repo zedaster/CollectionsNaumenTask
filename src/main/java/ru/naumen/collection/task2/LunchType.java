31c20b187caa90df6577cae4a7f806385f6449e8
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2023-10-20 12:38:01 +0500

    Complete task 2

---
+++ src/main/java/ru/naumen/collection/task2/LunchType.java
@@ -0,0 +1,7 @@
+package ru.naumen.collection.task2;
+
+public enum LunchType {
+    NO_LUNCH,
+    DRINKS,
+    FOOD_AND_DRINKS
+}
--- src/main/java/ru/naumen/collection/task2/Ticket.java
+++ src/main/java/ru/naumen/collection/task2/Ticket.java
@@ -1,5 +1,7 @@
 package ru.naumen.collection.task2;
 
+import java.util.Objects;
+
 /**
  * Билет
  *
@@ -9,4 +11,28 @@ package ru.naumen.collection.task2;
 public class Ticket {
     private long id;
     private String client;
+
+    /**
+     * Сравнивает содержимое этого объекта с любым другим
+     * @param o - объект, с которым сравнивается данный
+     * @return true, если класс Ticket и их id идентичны, иначе false
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id;
+    }
+
+    /**
+     * Вычисляет хэш-код объекта
+     * Необходимо для корректной работы в хэш-коллекциях
+     * Берется только id, т.к. он уникален
+     * @return хэш-код объекта
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
